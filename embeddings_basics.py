2e651591226277b9385437af4c1948a3a416e371
Rahul Garg <rahulgarg7042@gmail.com>
2025-11-13 23:35:57 +0530

    add embeddings

---
+++ embeddings_basics.py
@@ -0,0 +1,5 @@
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('all-MiniLM-L6-v2')
+sentences = ["I love AI", "Machine learning is fun", "Quantum physics rocks"]
+embeddings = model.encode(sentences)  # Shape: (3, 384)
+print(embeddings.shape)
