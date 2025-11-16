33e6b1f5a6cf3258e1e35df02bdd4d6667780fa4
Rahul Garg <rahulgarg7042@gmail.com>
2025-11-16 22:51:54 +0530

    minor update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ query = np.random.rand(1, 3)
 
 # Euclidean distance (brute force)
 distances = np.linalg.norm(vectors - query, axis=1)
+print("vectors:", vectors)
 print("Distances:", distances)
 closest_idx = np.argmin(distances)
 print("Closest vector:", vectors[closest_idx])
